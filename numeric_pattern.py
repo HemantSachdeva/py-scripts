984b0c7db6707996de84a13a862bf4ee53e7d5cf
Hemant Sachdeva <hemant.evolver@gmail.com>
2021-10-23 00:59:21 +0530

    numeri_pattern: Missing whitespace after ',', ';', or ':'
    Signed-off-by: Hemant Sachdeva <hemant.evolver@gmail.com>

--- numeric_pattern.py
+++ numeric_pattern.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python3 
+#!/usr/bin/env python3
 
 lst = []
-for i in range(1,6):
+for i in range(1, 6):
     for j in range(1, i+1):
         lst.append(j)
-    print(*lst,end="\n")
-    lst.clear() # clear list for next row
+    print(*lst, end="\n")
+    lst.clear()  # clear list for next row
