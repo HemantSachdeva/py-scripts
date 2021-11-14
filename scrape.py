6c28d7a8ca732a3eac679929bf45aa423ae5e1fa
Hemant Sachdeva <hemant.evolver@gmail.com>
2021-11-14 17:29:07 +0530

    Save scrapped table into a csv using `pandas`
    Signed-off-by: Hemant Sachdeva <hemant.evolver@gmail.com>

--- scrape.py
+++ scrape.py
@@ -3,8 +3,10 @@ import sys
 try:
     import requests
     from requests_html import HTML
+    import pandas as pd
 except ImportError:
-    sys.exit("[!] Please make sure you have installed requests and requests_html module")
+    sys.exit(
+        "[!] Please make sure you have installed requests, requests_html and pandas module.")
 
 
 def url_to_scrap(url, filename="scrapped_data.html"):
@@ -26,6 +28,7 @@ def parse_html(html_text):
     # find table with class imdb-scroll-table-inner in html
     table = html_text.find(".imdb-scroll-table-inner")
 
+    table_data = []
     # find all rows in table
     if len(table) == 1:
         parsed_table = table[0]
@@ -35,13 +38,20 @@ def parse_html(html_text):
         header = rows[0]
         header_cells = header.find("th")
         header_cells_text = [cell.text for cell in header_cells]
-        print(header_cells_text)
 
         # 2nd row onwards are data rows
         for row in rows[1:]:
-            row_data = row.find("td")
-            row_values = [x.text for x in row_data]
-            print(row_values)
+            cols = row.find("td")
+            row_data = []
+            # making list of lists for data so that any data can be easily converted to dataframe
+            # if data is having any ',' in it, it will now be considered as a list otherwise it will be a string
+            # and csv will read it as a delimiter
+            for i, col in enumerate(cols):
+                row_data.append(col.text)
+            table_data.append(row_data)
+
+        df = pd.DataFrame(table_data, columns=header_cells_text)
+        df.to_csv("scrapped_table.csv", index=False)
     else:
         print("[!] Error parsing table")
         sys.exit(1)
@@ -52,6 +62,12 @@ if __name__ == "__main__":
     html_text = url_to_scrap(URL)
     if html_text:
         print("[+] Scrapped data saved in scrapped_data.html")
-        parse_html(HTML(html=html_text))
     else:
         print("[!] Unable to scrap data")
+        sys.exit(1)
+
+    if parse_html(HTML(html=html_text)) is None:
+        print("[+] Scrapped data saved in scrapped_table.csv")
+    else:
+        print("[!] Unable to parse data")
+        sys.exit(1)
