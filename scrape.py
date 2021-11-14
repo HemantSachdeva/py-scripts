3df9fb5a49e272e8b628b821e25552539ba15548
Hemant Sachdeva <hemant.evolver@gmail.com>
2021-11-14 17:28:54 +0530

    Scrape only desired table using `requests_html`
    Signed-off-by: Hemant Sachdeva <hemant.evolver@gmail.com>

--- scrape.py
+++ scrape.py
@@ -2,8 +2,9 @@
 import sys
 try:
     import requests
+    from requests_html import HTML
 except ImportError:
-    sys.exit("[!] Please make sure you have installed requests module")
+    sys.exit("[!] Please make sure you have installed requests and requests_html module")
 
 
 def url_to_scrap(url, filename="scrapped_data.html"):
@@ -20,10 +21,37 @@ def url_to_scrap(url, filename="scrapped_data.html"):
         return None
 
 
+def parse_html(html_text):
+
+    # find table with class imdb-scroll-table-inner in html
+    table = html_text.find(".imdb-scroll-table-inner")
+
+    # find all rows in table
+    if len(table) == 1:
+        parsed_table = table[0]
+        rows = parsed_table.find("tr")
+
+        # 1st row is header
+        header = rows[0]
+        header_cells = header.find("th")
+        header_cells_text = [cell.text for cell in header_cells]
+        print(header_cells_text)
+
+        # 2nd row onwards are data rows
+        for row in rows[1:]:
+            row_data = row.find("td")
+            row_values = [x.text for x in row_data]
+            print(row_values)
+    else:
+        print("[!] Error parsing table")
+        sys.exit(1)
+
+
 if __name__ == "__main__":
     URL = "https://www.boxofficemojo.com/year/world/"
     html_text = url_to_scrap(URL)
     if html_text:
         print("[+] Scrapped data saved in scrapped_data.html")
+        parse_html(HTML(html=html_text))
     else:
         print("[!] Unable to scrap data")
